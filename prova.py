3865b15901918159296b54ab86cd5c361e8f2ab5
Stefano Cosentino <stefano286@gmail.com>
2016-04-20 17:59:38 +0100

    write to sd improved

--- prova.py
+++ prova.py
@@ -1,23 +1,10 @@
 __author__ = 'sc04'
 
-import datetime
-import time
-buf = bytearray(100)                # create a buffer of 100 bytes
-f = open("sadata.dat", "a")    # open the file for writing
-k=0
-lines=[]
-while (k<10):
-    k=k+1
-    [x, y, z] = [0,0,0]
-    #buf=[k,k,k,k,k,k,k,k,k,]
-    t = datetime.datetime.now().time()    # get time in ms
-    #f.write("{} {} {} {}\n".format(t, buf, x, y, z)) # write muscl_input and x,y,z
-        #f.write("{} {} {} {}\n".format(time, val, x, y, z)) # write muscl_input and x,y,z
-    lines.append('{} {} {} {}\n'.format(t,buf, x, y, z))# FASTER, BUT then lines gets quite big...
-    time.sleep(0.001)                    # apparently, for stability
-    #for val in buf:                # REPL: loop and print values
-    #    print(val)
-f.writelines(lines)
-f.close()                           # close the file
+string='asfdd fdf'
+def to_weird_case_word(string):
+    return "".join(c.upper() if i%2 == 0 else c for i, c in enumerate(string.lower()))
 
+def to_weird_case(string):
+    return " ".join(to_weird_case_word(str) for str in string.split())
 
+print(to_weird_case_word(string))
